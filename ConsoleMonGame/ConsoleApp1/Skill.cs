9536ddf9c3b4a1742dc921630c313ad2abc6882b
Teffer31 <t.barmak7@gmail.com>
2023-05-08 10:37:27 +0200

    02

---
+++ ConsoleMonGame/ConsoleApp1/Skill.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skill
+{
+    internal class Skill
+    {
+        int damage = 0;
+        int energy = 0;
+        string name = string.Empty;
+    }
+}
