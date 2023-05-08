d62e25607115dfb6970c7a2621e0087cfb85c235
Teffer31 <t.barmak7@gmail.com>
2023-05-08 10:27:57 +0200

    opdracht 01

---
+++ ConsoleMonGame/ConsoleApp1/ConsoleMon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMon
+{
+    internal class ConsoleMon
+    {
+        int health = 0;
+        int energy = 0;
+        string name = string.Empty;
+    }
+}
