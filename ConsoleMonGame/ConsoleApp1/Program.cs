4c8f79b779d913de3ba8687c6f885ad2888afa0e
Teffer31 <t.barmak7@gmail.com>
2023-06-08 15:12:07 +0200

    06

--- ConsoleMonGame/ConsoleApp1/Program.cs
+++ ConsoleMonGame/ConsoleApp1/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace ConsoleApp1
+﻿namespace Consolemon
 {
     internal class Program
     {
---
+++ console app/console app/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace console_app
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> birds = new List<string>() {"uil", "kraai", "hond", "papegaii" };
+            List<string> birds2 = new List<string>() {"meeuw", "duif" };
+            birds.AddRange(birds2);
+            birds.Remove("hond");
+
+            foreach (string bird in birds)
+            {
+                Console.WriteLine(bird);
+            }
+        }
+    }
+}
