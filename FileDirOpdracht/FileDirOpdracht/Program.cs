f6e7c36ab68f5d7cdf131673a15cefec14142e7f
Teffer31 <t.barmak7@gmail.com>
2023-06-29 12:18:48 +0200

    09 finished?

--- FileDirOpdracht/FileDirOpdracht/Program.cs
+++ FileDirOpdracht/FileDirOpdracht/Program.cs
@@ -5,6 +5,16 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\tbarm\OneDrive\Documenten\ma\mbo3\flex\c# opdrachten\2023P4ProgCs2\FileDirOpdracht");
+
+            string path = Path.GetTempFileName();
+            var fi1 = new FileInfo(path);
+
+            foreach (var fi in dir.GetFiles())
+            {
+                Console.WriteLine(fi.Name);
+            }
         }
     }
 }
